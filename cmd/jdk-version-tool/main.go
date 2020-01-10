3dc44e0173691c9684d8cd86d1aee9b0c85662ad
Manuel Fuchs <malax@malax.de>
2020-01-10 18:52:09 +0100

    WIP

--- cmd/jdk-version-tool/main.go
+++ cmd/jdk-version-tool/main.go
@@ -13,22 +13,24 @@ import (
 const defaultVendor = "openjdk"
 
 func main() {
-    switch os.Args[1] {
-    case "read-version-string":
-        readVersionStringOperation()
-    case "version-from-version-string":
-        versionFromVersionStringOperation()
-    case "vendor-from-version-string":
-        vendorFromVersionStringOperation()
+    operation := os.Args[1]
+
+    switch operation {
+    case "read-version-definition":
+        readVersionDefinitionOperation()
+    case "version-from-version-definition":
+        versionFromVersionDefinitionOperation()
+    case "vendor-from-version-definition":
+        vendorFromVersionDefinitionOperation()
     case "jdk-download-url":
         jdkDownloadUrlOperation()
     default:
-        _, _ = fmt.Fprintln(os.Stderr, "No operation specified. Valid values are: read-version-string, version-from-version-string, vendor-from-version-string and jdk-download-url")
+        _, _ = fmt.Fprintf(os.Stderr, "Unknown operation \"%s\" specified. Valid values are: read-version-definition, version-from-version-definition, vendor-from-version-definition and jdk-download-url", operation)
         os.Exit(1)
     }
 }
 
-func readVersionStringOperation() {
+func readVersionDefinitionOperation() {
     if propertiesFile, err := properties.LoadFile(os.Args[2], properties.UTF8); err == nil {
         if value, ok := propertiesFile.Get("java.runtime.version"); ok {
             fmt.Println(value)
@@ -39,14 +41,14 @@ func readVersionStringOperation() {
     os.Exit(1)
 }
 
-func versionFromVersionStringOperation() {
-    _, version := resolveVersionString(os.Args[2])
+func versionFromVersionDefinitionOperation() {
+    _, version := resolveVersionDefinition(os.Args[2])
     fmt.Println(version)
     os.Exit(0)
 }
 
-func vendorFromVersionStringOperation() {
-    vendor, _ := resolveVersionString(os.Args[2])
+func vendorFromVersionDefinitionOperation() {
+    vendor, _ := resolveVersionDefinition(os.Args[2])
     fmt.Println(vendor)
     os.Exit(0)
 }
@@ -84,13 +86,13 @@ func jdkDownloadUrlOperation() {
     }
 }
 
-func resolveVersionString(version string) (string, string) {
-    selectedVersion := version
+func resolveVersionDefinition(versionDefinition string) (string, string) {
+    selectedVersion := versionDefinition
     selectedVendor := defaultVendor
 
     switch selectedVersion {
     case "9.0.0", "9+181":
-        version = "9-181"
+        selectedVersion = "9-181"
         selectedVendor = "openjdk"
     default:
         vendorRegex := regexp.MustCompile("^([a-zA-Z]+?)-(.*)$")
