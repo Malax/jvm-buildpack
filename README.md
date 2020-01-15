1876fc620a3908a6968e89ec689716528b659156
Manuel Fuchs <malax@malax.de>
2020-01-15 17:12:02 +0100

    Add JDK caching and runtime setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Cloud Native JVM Buildpack
+
+## Environment Variables
+
+### BP_JVM_REINSTALL_JDK
+Forces the buildpack to reinstall the JDK, disregarding all caching mechanisms.
---
+++ opt/runtime/profile.d/jvm.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+calculate_dyno_size_based_opts() {
+  case $(ulimit -u) in
+  512)   # 2X, private-s: memory.limit_in_bytes=1073741824
+    echo "-Xmx671m -XX:CICompilerCount=2"
+    ;;
+  16384) # perf-m, private-m: memory.limit_in_bytes=2684354560
+    echo "-Xmx2g"
+    ;;
+  32768) # perf-l, private-l: memory.limit_in_bytes=15032385536
+    echo "-Xmx12g"
+    ;;
+  *) # Free, Hobby, 1X: memory.limit_in_bytes=536870912
+    echo "-Xmx300m -Xss512k -XX:CICompilerCount=2"
+    ;;
+  esac
+}
+
+calculate_java_version_based_opts() {
+  if grep -q '^JAVA_VERSION="1[0-9]' "${JAVA_HOME}/release"; then
+    echo "-XX:+UseContainerSupport"
+  else
+    echo ""
+  fi
+}
